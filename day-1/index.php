c5a0ddb189d7d8e7089552aeee1c152e527b9dbc
Liam Jackson <liam.jackson@agentsoftware.net>
2023-12-01 20:15:28 +0000

    fix: error.

--- day-1/index.php
+++ day-1/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$input = array_reduce(explode("\n", file_get_contents(__DIR__ . '/input.txt')), function ($input, $carry) {
+$input = array_reduce(explode("\n", file_get_contents(__DIR__ . '/input.txt')), function (int $carry, string $input) {
     $results = preg_replace("/[^0-9]/", "", $input);
     $length = strlen($results);
 
