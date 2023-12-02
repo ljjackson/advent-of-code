7a850650d7e22ac11fb0ad53bfefc9c25b829012
Liam Jackson <liam.jackson@agentsoftware.net>
2023-12-02 10:38:58 +0000

    solution-1.php

--- src/day-2/solution-1.php
+++ src/day-2/solution-1.php
@@ -1,5 +1,35 @@
 <?php
 
-function solutionOne(string $input) {
-    return 8;
+const MAX_BLUE = 14;
+const MAX_GREEN = 13;
+const MAX_RED = 12;
+
+function solutionOne(string $input): int
+{
+    return array_reduce(explode(PHP_EOL, $input), function (int $carry, string $gameInput) {
+        $numbers = preg_split('/([:;,])/', $gameInput);
+
+        // Determine the game number
+        $gameNumber = intval(explode(' ', $numbers[0])[1]);
+        unset($numbers[0]);
+
+        foreach ($numbers as $roundInput) {
+            $input = explode(' ', ltrim($roundInput));
+            $amount = intval($input[0]);
+            $colour = $input[1];
+
+            $impossibleGame = ($amount > MAX_RED && $colour === 'red')
+                || ($amount > MAX_GREEN && $colour === 'green')
+                || ($amount > MAX_BLUE && $colour === 'blue');
+
+            if ($impossibleGame) {
+                return $carry;
+            }
+        }
+
+        return $carry + $gameNumber;
+    }, 0);
 }
+
+
+var_dump(solutionOne(file_get_contents(__DIR__ . '/input.txt')));
