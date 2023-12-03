e221ed7781867f40ecf0c68d966565af1a1facf1
Liam Jackson <liam.jackson@agentsoftware.net>
2023-12-03 15:02:11 +0000

    feat: day-3

--- src/day-3/solutions.php
+++ src/day-3/solutions.php
@@ -41,12 +41,6 @@ function solutionOne(string $input): float|int
             $current = substr($engine[$currentLine], $firstIndex, $length);
             $below = substr($engine[$currentLine + 1] ?? '', $firstIndex, $length);
 
-            if ($currentNumber === '58') {
-                var_dump($above);
-                var_dump($current);
-                var_dump($below);
-            }
-
             $isPartNumber = containsSymbol(
                 $above . $below . $current
             );
@@ -74,4 +68,149 @@ function containsSymbol(string $input): bool
     return strlen(preg_replace('/[0-9|.]/', '', $input)) > 0;
 }
 
-var_dump(solutionOne(file_get_contents(__DIR__ . '/input.txt')));
+
+function solutionTwo(string $input): int
+{
+    $engine = explode(PHP_EOL, $input);
+
+    $currentLine = 0;
+    $totalLines = count($engine);
+    $currentLetter = 0;
+    $lettersPerLine = strlen($engine[0]);
+    $gearProducts = [];
+    $allNumbers = [];
+
+    while ($currentLine < $totalLines) {
+        $isLastLetterInLine = ($currentLetter + 1) === $lettersPerLine;
+        $value = $engine[$currentLine][$currentLetter];
+        $startingOnFirstLine = $currentLetter - 1 <= 0;
+        $isStartingOnLastLine = $currentLetter + 1 > $lettersPerLine - 1;
+
+        if ($value === '*') {
+            // Create a box surrounding the asterisk
+            // The top left will be true if there is a number there
+            // |TFF|
+            // |FFF|
+            // |FFF|
+            $box = [
+                'top' => ($currentLine - 1) < 0 ? [false, false, false] : [
+                    ! $startingOnFirstLine && is_numeric($engine[$currentLine - 1][$currentLetter - 1]),
+                    is_numeric($engine[$currentLine - 1][$currentLetter]),
+                    ! $isStartingOnLastLine && is_numeric($engine[$currentLine - 1][$currentLetter + 1])
+                ],
+                'middle' => [
+                    ! $startingOnFirstLine && is_numeric($engine[$currentLine][$currentLetter - 1]),
+                    false,
+                    ! $isStartingOnLastLine && is_numeric($engine[$currentLine][$currentLetter + 1])
+                ],
+                'bottom' => $currentLine === ($totalLines - 1) ? [false, false, false] : [
+                    ! ($startingOnFirstLine) && is_numeric($engine[$currentLine + 1][$currentLetter - 1]),
+                    is_numeric($engine[$currentLine + 1][$currentLetter]),
+                    ! $isStartingOnLastLine && is_numeric($engine[$currentLine + 1][$currentLetter + 1])
+                ],
+            ];
+
+            if (hasMultipleNumbersInSurroundingBox($box)) {
+                $numbers = [];
+
+                foreach ($box as $area => $options) {
+                    if (($area === 'top' && ($currentLine - 1) < 0) || ($area === 'bottom' && $currentLine === ($totalLines - 1))) {
+                        continue;
+                    }
+
+                    $index = $currentLine;
+                    if ($area === 'top') {
+                        $index = $currentLine - 1;
+                    }
+                    if ($area === 'bottom') {
+                        $index = $currentLine + 1;
+                    }
+
+                    $searchBackwardsNumber = '';
+                    $searchForwardNumber = '';
+
+                    if ($options[0]) {
+                        $searchIndex = $currentLetter - 1;
+                        while (isset($engine[$index][$searchIndex]) && is_numeric($engine[$index][$searchIndex])) {
+                            $searchBackwardsNumber = $engine[$index][$searchIndex] . $searchBackwardsNumber;
+                            $searchIndex--;
+                        }
+                    }
+
+                    if ($options[2]) {
+                        $searchIndex = $currentLetter + 1;
+                        while (isset($engine[$index][$searchIndex]) && is_numeric($engine[$index][$searchIndex])) {
+                            $searchForwardNumber .= $engine[$index][$searchIndex];
+                            $searchIndex++;
+                        }
+                    }
+
+                    if ($options[1]) {
+                        $numbers[] = intval(
+                            $searchBackwardsNumber . $engine[$index][$currentLetter] . $searchForwardNumber
+                        );
+                        continue;
+                    }
+
+                    if (strlen($searchForwardNumber) > 0) {
+                        $numbers[] = intval($searchForwardNumber);
+                    }
+
+                    if (strlen($searchBackwardsNumber) > 0) {
+                        $numbers[] = intval($searchBackwardsNumber);
+                    }
+                }
+
+                $gearProducts[] = array_product($numbers);
+            }
+        }
+
+
+        $currentLetter++;
+
+        if ($isLastLetterInLine) {
+            $currentLetter = 0;
+            $currentLine++;
+        }
+    }
+
+    return array_sum($gearProducts);
+}
+
+
+function hasMultipleNumbersInSurroundingBox(array $values): bool
+{
+    $total = 0;
+
+    if (count(array_filter($values['top'])) > 0) {
+        $total += 1;
+    }
+
+    if (count(array_filter($values['middle'])) > 0) {
+        $total += 1;
+    }
+
+    if (count(array_filter($values['bottom'])) > 0) {
+        $total += 1;
+    }
+
+    if ($total > 1) {
+        return true;
+    }
+
+    if ($values['top'][0] && ! $values['top'][1] && $values['top'][2]) {
+        return true;
+    }
+
+    if ($values['middle'][0] && ! $values['middle'][1] && $values['middle'][2]) {
+        return true;
+    }
+
+    if ($values['bottom'][0] && ! $values['bottom'][1] && $values['bottom'][2]) {
+        return true;
+    }
+
+    return false;
+}
+
+var_dump(solutionTwo(file_get_contents(__DIR__ . '/input.txt')));
--- tests/Unit/Day3Test.php
+++ tests/Unit/Day3Test.php
@@ -10,4 +10,20 @@ class Day3Test extends TestCase
     {
         $this->assertSame(4361, solutionOne(file_get_contents(__DIR__ . '/../fixtures/day3.txt')));
     }
+
+    public function test_it_will_return_correct_value_with_counting_edges()
+    {
+        $this->assertSame(160, solutionTwo(file_get_contents(__DIR__ . '/../fixtures/day3/160.txt')));
+    }
+
+    public function test_it_will_return()
+    {
+        $this->assertSame(305404, solutionTwo(file_get_contents(__DIR__ . '/../fixtures/day3/first_line.txt')));
+        $this->assertSame(5565451, solutionTwo(file_get_contents(__DIR__ . '/../fixtures/day3/5565451.txt')));
+    }
+
+    public function test_performance()
+    {
+        $this->assertSame(79026871, solutionTwo(file_get_contents(__DIR__ . '/../../src/day-3/input.txt')));
+    }
 }
