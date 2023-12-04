6f037c88fd565601d419d521bcd28a0ce75d53b3
Liam Jackson <liam.jackson@agentsoftware.net>
2023-12-04 20:54:46 +0000

    day 3 part 2

--- src/day-3/solutions.php
+++ src/day-3/solutions.php
@@ -23,3 +23,37 @@ function solutionOne(string $input)
 
     return $sum;
 }
+
+
+
+function solutionTwo(string $input): int
+{
+    $dict = array_flip(array_merge(range('a', 'z'), range('A', 'Z')));
+    $inputs = explode(PHP_EOL, $input);
+
+    $sum = 0;
+    $group = [];
+
+    foreach ($inputs as $index => $input) {
+        $isThirdIteration = ($index + 1) % 3 === 0;
+        $group[] = $input;
+
+
+        if ($isThirdIteration) {
+            $groups = array_map('str_split', $group);
+            $common = array_intersect(...$groups);
+            $letters = array_values(array_unique(array_intersect(...$groups)));
+
+            if ($letters[0]) {
+                $sum += ($dict[$letters[0]] + 1);
+            }
+
+
+            $group = [];
+        }
+
+
+    }
+
+    return $sum;
+}
--- tests/Day3Test.php
+++ tests/Day3Test.php
@@ -9,6 +9,11 @@ class Day3Test extends \PHPUnit\Framework\TestCase
         $this->assertSame(157, solutionOne(file_get_contents(__DIR__ . '/fixtures/day-3/test.txt')));
         $this->assertSame(7967, solutionOne(file_get_contents(__DIR__ . '/fixtures/day-3/actual.txt')));
     }
+    public function test_solution_two()
+    {
+        $this->assertSame(70, solutionTwo(file_get_contents(__DIR__ . '/fixtures/day-3/test.txt')));
+        $this->assertSame(2716, solutionTwo(file_get_contents(__DIR__ . '/fixtures/day-3/actual.txt')));
+    }
 
 
 }
