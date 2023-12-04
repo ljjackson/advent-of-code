52240ac5717b984b5d1a1a44098dd3cb46448f5d
Liam Jackson <liam.jackson@agentsoftware.net>
2023-12-04 05:07:12 +0000

    day-2 wip

---
+++ src/day-2/solutions.php
@@ -0,0 +1,61 @@
+<?php
+
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
+const OPPONENT = [
+    'A' => ROCK,
+    'B' => PAPER,
+    'C' => SCISSORS,
+];
+
+const PLAYER = [
+    'Y' => PAPER,
+    'X' => ROCK,
+    'Z' => SCISSORS,
+];
+
+const WIN = 6;
+const LOSS = 0;
+const DRAW = 3;
+
+const POINTS = [
+    PAPER => 2,
+    ROCK => 1,
+    SCISSORS => 3,
+];
+
+const WINNING_RESULTS = [
+    ROCK => SCISSORS,
+    SCISSORS => PAPER,
+    PAPER => ROCK,
+];
+
+function determineGame(string $player, string $opponent): int
+{
+    $player = PLAYER[$player];
+    $opponent = OPPONENT[$opponent];
+    $defaultPoints = POINTS[$player];
+
+    // Draw...
+    if ($opponent === $player) {
+        return DRAW + $defaultPoints;
+    }
+
+    // Wins...
+    if (WINNING_RESULTS[$player] === $opponent) {
+        return WIN + $defaultPoints;
+    }
+
+    return LOSS + $defaultPoints;
+}
+
+function solutionOne(string $input): int
+{
+    return array_reduce(explode(PHP_EOL, $input), function (int $carry, string $input) {
+        $results = explode(' ', $input);
+
+        return determineGame($results[1], $results[0]);
+    }, 0);
+}
---
+++ tests/Day2Test.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Day2Test extends TestCase
+{
+    public function test_it()
+    {
+        
+    }
+}
